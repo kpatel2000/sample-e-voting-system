9c232bb91d9485edb5ce155b80116197c5ee7b1b
Pranshu-Raj <pranshuraj828@gmail.com>
2022-11-19 23:58:10 +0530

    first commit

---
+++ front-end/src/pages/results/data.ts
@@ -0,0 +1,14 @@
+export const states: any = {
+  "10": "Andhra Pradesh",
+  "11": "Bihar",
+};
+export const constituency: any = {
+  "10": {
+    "1": "Guntur",
+    "2": "Krishna",
+  },
+  "11": {
+    "1": "Patna",
+    "2": "Dehri",
+  },
+};
